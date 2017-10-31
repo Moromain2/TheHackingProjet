1b8279e2af593ab16060488acb27a6170ea3821e
Romain Mosser <mosser.romain@gmail.com>
2017-10-31 15:12:10 +0100

    ajout exercice 31/10

--- members-2/db/seeds.rb
+++ members-2/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"loulou",email:"lou@lou.com",password_digest:"hdfbhYzbnfs71")
